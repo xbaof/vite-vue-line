47f562c785592ff9d8ba5855624c87ed271c072f
xbaof <xiaogu060@163.com>
2024-05-29 19:16:51 +0800

    update stylelint

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,13 +1,13 @@
+// @see https://stylelint.bootcss.com/
 module.exports = {
-  /* 继承某些已有的规则 */
   extends: [
+    'stylelint-config-standard',
+    'stylelint-config-html/vue',
     'stylelint-config-standard-scss',
     'stylelint-config-recommended-vue/scss',
-    'stylelint-config-html/vue',
     'stylelint-config-recess-order'
   ],
   overrides: [
-    // 扫描 .vue/html 文件中的<style>标签内的样式
     {
       files: ['**/*.{vue,html}'],
       customSyntax: 'postcss-html'
@@ -17,20 +17,27 @@ module.exports = {
       customSyntax: 'postcss-scss'
     }
   ],
-  /**
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts', '**/*.json', '**/*.md', '**/*.yaml'],
+  /*
    * null  => 关闭该规则
+   * always => 必须
    */
   rules: {
+    'font-family-no-missing-generic-family-keyword': null,
+    'value-keyword-case': null, // 在 css 中使用 v-bind，不报错
+    'no-descending-specificity': null, // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
     'function-url-quotes': 'always', // 要求或禁止 URL 的引号 "always(必须加上引号)"|"never(没有引号)"
-    'string-quotes': 'single', // 指定字符串使用单引号或双引号
-    'selector-class-pattern': null, // 强制选择器类名的格式
-    'unit-case': 'lower', // 指定单位的大小写 "lower(全小写)"|"upper(全大写)"
-    'color-hex-case': 'lower', // 指定 16 进制颜色的大小写 "lower(全小写)"|"upper(全大写)"
+    'no-empty-source': null, // 关闭禁止空源码
+    'selector-class-pattern': null, // 关闭强制选择器类名的格式
+    'property-no-unknown': null, // 禁止未知的属性(true 为不允许)
+    'value-no-vendor-prefix': null, // 关闭 属性值前缀 --webkit-box
+    'property-no-vendor-prefix': null, // 关闭 属性前缀 -webkit-mask
     'color-hex-length': 'long', // 指定 16 进制颜色的简写或扩写 "short(16进制简写)"|"long(16进制扩写)"
     'selector-pseudo-class-no-unknown': [
+      // 不允许未知的选择器
       true,
       {
-        ignorePseudoClasses: ['global', 'v-deep', 'deep', 'export']
+        ignorePseudoClasses: ['global', 'v-deep', 'deep', 'export'] // 忽略属性，修改element默认样式的时候能使用到
       }
     ]
   }
